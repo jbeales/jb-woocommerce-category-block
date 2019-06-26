8d176efffe057e79572533940f39032177b513e5
John Beales <john@johnbeales.com>
2019-06-26 09:04:59 -0400

    Initial plugin header.

---
+++ woocommerce-category-block.php
@@ -0,0 +1,15 @@
+<?php
+
+/*
+Plugin Name: Product Category Block for WooCommerce
+Plugin URI: 
+Description: Adds an editor block to link to a specific product category.
+Author: John Beales
+Author URI: http://johnbeales.com
+License: GPL2
+Text Domain: jb-wcb
+
+*/
+
+
+
